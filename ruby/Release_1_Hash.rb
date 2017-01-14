3d62a73c3952ce07d88e0e3f4191c21ba99d7e63
tsteinhause <tsteinhause@gmail.com>
2017-01-14 14:18:29 -0800

    added hash

---
+++ ruby/Release_1_Hash.rb
@@ -0,0 +1,26 @@
+puts "Name"
+user_name = gets.chomp
+
+puts "Address"
+address = gets.chomp
+
+puts "email"
+address = gets.chomp
+
+puts "phone"
+phone = gets.chomp
+
+
+
+
+
+
+Interior = {
+	:Name 
+	:Address 
+	:Email 
+	:Phone 
+	:Blue 
+	:Wallpaper
+	:Ombre
+}
