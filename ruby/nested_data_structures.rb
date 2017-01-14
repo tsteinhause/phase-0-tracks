2e7326ce05447075bd38434b827f45b2ca77227b
tsteinhause <tsteinhause@gmail.com>
2017-01-14 14:17:56 -0800

    Built nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,35 @@
+amazon_store = {
+	clothing: [
+		"mens_clothing",
+		"womens_clothing",
+		"childrens_clothing",
+		"watches" {
+			mens_watches: 2,
+			womens_watches: 4,
+		}
+	]
+
+	electronics: {
+		"video_games"
+		"TVs"
+		"computers" {
+			total: 200,
+			remaining: 42,
+		}
+
+	}
+
+	movies_and_music: {
+		music_genres: [
+			"hip hop",
+			"country",
+			"dance",
+		]
+		movie_genres: [
+			"comedy",
+			"horror",
+			"romance",
+		]
+
+	}
+}
