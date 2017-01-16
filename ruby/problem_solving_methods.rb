1a2b33aa09dc5b6e60c974ac77912ad73185a9ea
tsteinhause <tsteinhause@gmail.com>
2017-01-15 18:21:16 -0800

    added bubble sorting method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -3,3 +3,25 @@ def fib(int)
 end
 
 p fib(100)
+
+# Bubble Sort
+# a simple sorting algorithm that repeatedly sort through the problem
+# it's slower than other sorting methods, because it goes through the entire list every time
+# and compares each pair of adjacent items and swaps if they are in the wrong order
+
+
+def bubbleSort(list)
+   sorted = false
+   until sorted
+     sorted = true
+     for i in 0..(list.length - 2)
+       if list[i] > list[i + 1]
+         sorted = false
+         list[i], list[i + 1] = list[i + 1], list[i]
+       end
+     end
+   end
+   return list
+ end 
+
+ p bubblesort([3,1,90,30,100,2,50])
