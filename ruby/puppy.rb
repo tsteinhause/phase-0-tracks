e539b7b38db586ad5313ad11b85296fd7d571836
tsteinhause <tsteinhause@gmail.com>
2017-01-18 12:47:00 -0800

    Updated puppy.rb file with classes

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+#SPECIES ---------------
+#Boxer
+
+#CHARACTERISTICS -------
+
+#Large
+#Muscular
+#Square head
+#Brown Body
+#White Chest 
+
+#BEHAVIOR --------------
+
+#Playful
+#Energetic
+#Brave
+#Intelligent
+#Loyal
