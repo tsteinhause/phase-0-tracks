1b0c1d31c23d9cce889ec13a284f9f8585fa0fc1
tsteinhause <tsteinhause@gmail.com>
2017-01-30 09:30:16 -0800

    attempted to finish the file but could not understand the last release

---
+++ js/algos.js
@@ -0,0 +1,29 @@
+var phrases = ["long phrase","longest phrase","longer phrase"];
+
+// set phrase length to 0
+var longest = 0;
+// create method that as long as the variable is less than the length of the number of string, it will operate 
+for (var i = 0; i < [phrases].length; i++) {
+if (longest < phrases[i].length) {
+    longest = phrases[i];
+  }
+}
+
+console.log(longest)
+
+
+// Create the object
+var nameAge1 = {name: "Steven", age: 54};
+var nameAge2 = {name: "Tamir", age: 54};
+
+function check(nameAge1, nameAge2){
+   for (var i in nameAge1) {
+           if (! nameAge2.hasOwnProperty(i) || nameAge1[i] !== nameAge2[i] ) {
+              return false;
+           }       
+   }
+   return true;
+}
+
+console.log(check)
+
