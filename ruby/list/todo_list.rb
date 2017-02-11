6a8f4c28e9629f735a94babdab5ea46919a98f67
tsteinhause <tsteinhause@gmail.com>
2017-02-11 18:08:54 -0500

    Added a calculator file that could be tested by rspec

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,26 @@
+class TodoList
+  def initialize(list)
+    @list = list
+  end  
+  
+  def get_items
+    @list
+  end
+  
+  def add_item(item)
+   @list << item 
+  end
+  
+  def delete_item(item)
+    @list.delete(item)
+    @list
+  end
+  
+  def get_item(i)
+    @list[i]
+  end  
+end
+
+todolist = TodoList.new(["Mow the grass","Mop the floor"])
+todolist.add_item("ab")
+todolist.delete_item("ab")
