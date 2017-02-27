e065fbe9ea06847e4388893358049edb59726ba5
tsteinhause <tsteinhause@gmail.com>
2017-02-27 12:44:56 -0500

    added a calendar feature that puts in a quote for every day that the user chooses

--- databases/kitten_maker/pre_kitten_maker.rb
+++ databases/kitten_maker/pre_kitten_maker.rb
@@ -12,11 +12,11 @@ db.results_as_hash = true
 
 # learn about fancy string delimiters
 create_table_cmd = <<-SQL
-	CREATE TABLE IF NOT EXISTS kittens(
-	id INTEGER PRIMARY KEY,
-	name VARCHAR(255),
-	age INT
-	)
+  CREATE TABLE IF NOT EXISTS kittens(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+  )
 SQL
 
 # create a kittens table (if it's not there already)
@@ -24,27 +24,21 @@ db.execute(create_table_cmd)
 
 # add a test kitten
 # db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
-db.execute("INSERT INTO kittens (name, age) VALUES ('bob', 10)")
 
 # add LOOOOTS of kittens!
 # so. many. kittens. 
 #KittenExplosion
-
 def create_kitten(db, name, age)
-	db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
-end 	
+  db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+end
 
-1000.times do
-	create_kitten(db, , 0)
-end	
+10000.times do
+  create_kitten(db, Faker::Name.name, 0)
+end
 
+# explore ORM by retrieving data
 # kittens = db.execute("SELECT * FROM kittens")
 # kittens.each do |kitten|
 #  puts "#{kitten['name']} is #{kitten['age']}"
 # end
 
-# explore ORM by retrieving data
-#kittens  = db.execute("SELECT * FROM kittens")
-#kittens.each do |kitten|
-#	puts "#{kitten['name']} is #{kitten['age']}"
-#end 	
