80f051f508f569d2f63e5f703a33d194921442d9
tsteinhause <tsteinhause@gmail.com>
2017-03-06 07:31:15 -0800

    added 9.4 answers

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -40,7 +40,27 @@ end
 # write a GET route that retrieves
 # a particular student
 
+
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+get '/contact' do
+  "6611 Sueno Rd.<br>
+  Santa Barbara, CA 93117<br>"
+end
+
+et '/great_job/' do
+  person = params[:name]
+  if person
+  "Good job #{person}!"
+  else
+  "Good job!"
+  end
+end
+
+get '/add/:num_1/and/:num_2' do
+  number_1 = params[:num_1].to_i
+  number_2 = params[:num_2].to_i
 end
